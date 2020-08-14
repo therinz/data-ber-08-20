17c3da9f207bbca6b942c903b81ba9f6a2c00e7e
Rinze Douma <rjcdouma@gmail.com>
2020-08-14 16:19:37 +0200

    Finished game methods prior testing for bugs

--- Projects/module-1_projects/01_python-project/your-project/blackjack.py
+++ Projects/module-1_projects/01_python-project/your-project/blackjack.py
@@ -52,34 +52,40 @@ class Deck:
 
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, cash, bet):
         """Class to store name, cards drawn and score of Player."""
 
         self.hand = []
         self.score = 0
         self.name = name
+        self.bet = bet
+        self.cash = cash
 
 
 class Game:
     """Initialize and execute blackjack game."""
 
-    def __init__(self, num_players=2, mode="casino"):
+    def __init__(self, num_players=2, cash=1000, min_bet=100):
         """Initialize values for game."""
 
         name_q = "What's the name of player number {}?"
         self.players = []
         for p in range(num_players):
             name = validate_input(name_q.format(str(p+1)))
-            self.players[p] = Player(name)
+            self.players[p] = Player(name, cash, min_bet)
         self.shoe = Deck()
-        self.num_players = num_players
+        self.active = list(self.players)
+        self.dealer = Player("Dealer", 1000000, 0)
 
-    def deal_card(self, player):
-        # deal card
+    def deal_card(self, player, first=False):
+        """Deal card, check if ace and check score."""
+
+        # Deal card
         new_card = self.shoe.deal()
+        if first:
+            # First round: return second card
+            return self.shoe.deal()
         # check score
-        if len(player.hand) < 3:
-            return
         if new_card.value == 14:
             new_card.value = self.ask_ace(new_card)
         player.hand.append(new_card)
@@ -90,12 +96,12 @@ class Game:
         """Ask whether ace counts as 1 or 11."""
 
         p = (f"You drew an Ace of {Card.suits[c.suit]}. "
-             f"Does it count as 1) or 11?")
+             f"Does it count as 1 or 11?")
         choice = validate_input(p, int, options=[1, 11])
         return 1 if choice == 1 else 14
 
     def check_score(self, player):
-        """Returns sum of cards in current hand."""
+        """Updates sum of cards in current hand."""
 
         for card in player.hand:
             if card == 14:
@@ -106,33 +112,95 @@ class Game:
                 player.score += card
 
     def check_natural(self, player):
+        """Check whether player hand qualifies as natural."""
+
         a, b = player.hand
         if ((a.value in [10, 11, 12, 13] and b.value == 14)
                 or (b.value in [10, 11, 12, 13] and a.value == 14)):
-            return True
+            player.score = 21
 
-    def update_bet(self):
+    def update_bet(self, natural=False):
         """Calculate win/lose and update stack"""
-        pass
 
-    def play(self):
-        """Run the game."""
-        # deal first hand
+        # First set dealer score to zero if bust
+        dealer = self.dealer.score if self.dealer.score < 22 else 0
+
+        # For every player check outcome
         for p in self.players:
-            for i in range(2):
-                self.deal_card(p)
-            if self.check_natural(p):
-                # what to do for natural
-                pass
+            # Only execute if this player or dealer has natural
+            if not natural or (natural and dealer != 21):
+                continue
+
+            if p.score < dealer or p.score > 21:
+                p.cash -= p.bet
+                self.dealer.cash += p.bet
+            elif p.score > dealer:
+                # If player has natural but dealer doesn't then bet * 1,5
+                if natural:
+                    p.bet *= 1.5
+                p.cash += p.bet
+                self.dealer.cash -= p.bet
+
+    def first_round(self):
+        """Deal first hand to all players and dealer."""
+
+        for p in self.players:
+            self.deal_card(p, first=True)
+            self.check_natural(p)
+
             # Check if ace in first hand
             for card in p.hand:
                 if card.value == 14:
                     self.ask_ace(card)
+
         # dealer first cards
-        # check natural
-        # for each player deal
-        # reshuffle
-        pass
+        dealer = self.dealer
+        self.deal_card(dealer, first=True)
+        self.check_natural(dealer)
+        if any([p.score for p in self.players] + [self.dealer.score]):
+            self.update_bet(natural=True)
+
+    def play_round(self):
+        """After first cards each players goes a round"""
+
+        prompt = "Stand (s) or hit (h)?"
+        s_or_h = ["stand", "hit", "s", "h"]
+
+        for p in self.active:
+            while p.score < 21:
+                q = validate_input(prompt, str, options=s_or_h)
+                if q in ["hit", "h"]:
+                    self.deal_card(p)
+                else:
+                    self.active.remove(p)
+                    break
+
+        # Dealer must stand when 17 or over
+        d = self.dealer
+        while d.score < 17:
+            self.deal_card(d)
+            # Ace counts as 11 when total would be between 16 & 22
+            if 1 in d.hand:
+                if 16 < sum(d.hand + [10]) < 22:
+                    break
+
+    def reset_players(self):
+        for p in self.players:
+            p.hand.clear()
+            p.score = 0
+        self.active = list(self.players)
+
+    def play(self):
+        """Run the game and clean up after"""
+
+        self.first_round()
+        self.play_round()
+        self.update_bet()
+        self.reset_players()
+
+        # If blank card has been reached then reshuffle
+        if self.shoe.blank:
+            self.shoe.reshuffle()
 
 
 def validate_input(prompt, type_=None, min_=None, max_=None, options=None):
@@ -164,7 +232,7 @@ def validate_input(prompt, type_=None, min_=None, max_=None, options=None):
             print(f"Input must be less than or equal to {max_}.")
         elif min_ is not None and ui_num < min_:
             print(f"Input must be more than or equal to {min_}.")
-        elif options is not None and ui_num not in options:
+        elif options is not None and ui not in options:
             print(f"Input must be one of the following:" + " ".join(options))
         else:
             return ui
